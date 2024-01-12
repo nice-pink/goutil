b18986c77fd60936fec0a34536b7fd02b24fa0a6
nice-pink <r@nice.pink>
2024-01-12 09:52:04 +0100

    Add log pkg.

---
+++ pkg/log/log.go
@@ -0,0 +1,18 @@
+package log
+
+import "fmt"
+
+func Info(logs ...any) {
+	//params := append([]any{" "}, logs...)
+	fmt.Println(logs...)
+}
+
+func Warn(logs ...any) {
+	params := append([]any{"⚠️ Warn: "}, logs...)
+	fmt.Println(params...)
+}
+
+func Error(logs ...any) {
+	params := append([]any{"❌ Error: "}, logs...)
+	fmt.Println(params...)
+}
