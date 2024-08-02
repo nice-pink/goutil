56e586efb29abcfc9e21da57bee202551a32d88a
nice-pink <r@nice.pink>
2024-08-02 14:09:23 +0200

    feat: Add log level to log.

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -6,29 +6,58 @@ import (
 	"time"
 )
 
+const (
+	Reset   = "\033[0m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Magenta = "\033[35m"
+	Cyan    = "\033[36m"
+	Gray    = "\033[37m"
+	White   = "\033[97m"
+)
+
 func Newline() {
 	fmt.Println()
 }
 
+func Debug(logs ...any) {
+	params := append([]any{time.Now().Format(time.DateTime) + " DEBUG:"}, logs...)
+	fmt.Println(params...)
+}
+
 func Info(logs ...any) {
-	params := append([]any{time.Now().Format(time.DateTime) + " ->"}, logs...)
+	params := append([]any{time.Now().Format(time.DateTime) + " INFO:"}, logs...)
 	fmt.Println(params...)
 }
 
 func Warn(logs ...any) {
-	params := append([]any{time.Now().Format(time.DateTime) + " ->", "🔶 Warn:"}, logs...)
+	params := append([]any{Yellow + time.Now().Format(time.DateTime) + " WARNING:" + Reset}, logs...)
 	fmt.Println(params...)
 }
 
 func Error(logs ...any) {
-	params := append([]any{time.Now().Format(time.DateTime) + " ->", "❌ Error:"}, logs...)
+	params := append([]any{Red + time.Now().Format(time.DateTime) + " ERROR:" + Reset}, logs...)
 	fmt.Println(params...)
 }
 
 func Err(err error, logs ...any) {
-	params := append([]any{time.Now().Format(time.DateTime) + " ->", "❌ Error:"}, logs...)
+	params := append([]any{Red + time.Now().Format(time.DateTime) + " ERROR:" + Reset}, logs...)
 	fmt.Println(params...)
 	fmt.Println(err)
+}
+
+func Critical(logs ...any) {
+	params := append([]any{Red + time.Now().Format(time.DateTime) + " ERROR:" + Reset}, logs...)
+	fmt.Println(params...)
+}
+
+// special
+
+func Head(logs ...any) {
+	params := append([]any{Red + time.Now().Format(time.DateTime) + " :" + Reset}, logs...)
+	fmt.Println(params...)
 }
 
 func Time() {
@@ -41,7 +70,7 @@ func Flags(withNewLine bool) {
 	}
 	fmt.Println("Flags:")
 	flag.VisitAll(func(f *flag.Flag) {
-		fmt.Printf("-%s: %s\n", f.Name, f.Value)
+		fmt.Printf(Blue+"-%s: %s\n"+Reset, f.Name, f.Value)
 	})
 	if withNewLine {
 		fmt.Println()
