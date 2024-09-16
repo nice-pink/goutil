7626f711a4d698192d5d113e4c5e11a5af037ab4
nice-pink <r@nice.pink>
2024-09-16 14:33:58 +0200

    feat: differ up/download and up/download to.

--- pkg/network/file.go
+++ pkg/network/file.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"net/http"
@@ -9,7 +10,7 @@ import (
 	"github.com/nice-pink/goutil/pkg/log"
 )
 
-func DownloadHttp(url string, filepath string) error {
+func DownloadHttpTo(url string, filepath string) error {
 	log.Info("http download:", url)
 
 	out, err := os.Create(filepath)
@@ -41,7 +42,27 @@ func DownloadHttp(url string, filepath string) error {
 	return nil
 }
 
-func UploadHttp(url string, filepath string, contentType string) error {
+func DownloadHttp(url string) ([]byte, error) {
+	log.Info("http download:", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Err(err, "Could not request url.")
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		log.Error("bad status: %s", resp.Status)
+		return nil, errors.New("bad status: " + resp.Status)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	return data, err
+}
+
+func UploadHttpFrom(url string, filepath string, contentType string) error {
 	log.Info("http upload", filepath, "to", url, "with content type", contentType)
 
 	file, err := os.Open(filepath)
@@ -51,6 +72,26 @@ func UploadHttp(url string, filepath string, contentType string) error {
 	}
 
 	req, err := http.NewRequest(http.MethodPut, url, file)
+	if err != nil {
+		log.Err(err, "Could not create put request.")
+		return err
+	}
+	req.Header.Add("Content-Type", contentType)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Err(err, "Could not send request.")
+		return err
+	}
+	defer res.Body.Close()
+	return nil
+}
+
+func UploadHttp(url, contentType string, data []byte) error {
+	log.Info("http upload", url, "with content type", contentType)
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(data))
 	if err != nil {
 		log.Err(err, "Could not create put request.")
 		return err
