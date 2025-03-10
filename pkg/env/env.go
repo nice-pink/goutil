d1590846946a683caeb502bb18ff063edb07f01e
nice-pink <r@nice.pink>
2025-03-10 20:22:11 +0100

    feat: Add get env with default.

---
+++ pkg/env/env.go
@@ -0,0 +1,40 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetEnvString(key, defaultValue string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return defaultValue
+}
+
+func GetEnvInt(key string, defaultValue int) int {
+	if v := os.Getenv(key); v != "" {
+		if i, err := strconv.Atoi(v); err == nil {
+			return i
+		}
+	}
+	return defaultValue
+}
+
+func GetEnvBool(key string, defaultValue bool) bool {
+	if v := os.Getenv(key); v != "" {
+		if b, err := strconv.ParseBool(v); err == nil {
+			return b
+		}
+	}
+	return defaultValue
+}
+
+func GetEnvFloat64(key string, defaultValue float64) float64 {
+	if v := os.Getenv(key); v != "" {
+		if f, err := strconv.ParseFloat(v, 64); err == nil {
+			return f
+		}
+	}
+	return defaultValue
+}
